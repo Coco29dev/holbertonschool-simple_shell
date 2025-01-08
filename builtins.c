05883f4899ade58fe318d34ae2570d653aaee6d3
Coco29dev <10059@holbertonstudents.com>
2025-01-08 14:47:30 +0000

    modification builtins.c pour exit

--- builtins.c
+++ builtins.c
@@ -33,16 +33,15 @@ int handle_builtin(char **cmd_args)
 	if (cmd_args == NULL || cmd_args[0] == NULL)
 	return (0);
 
-	if (strcmp(cmd_args[0], "env") == 0)
-	{
-		env_builtin();
-		return (1);
-	}
-
 	if (strcmp(cmd_args[0], "exit") == 0)
 	{
-	exit(0);
+		exit(0);
 	}
+	if (strcmp(cmd_args[0], "env") == 0)
+        {
+                env_builtin();
+                return (1);
+        }
 
 	return (0);
 }
