f6d5648faf6aea5553b466e01e39fed2596ace92
Coco29dev <10059@holbertonstudents.com>
2025-01-09 13:56:39 +0000

    ajout commentaire exit.c

--- exit.c
+++ exit.c
@@ -1,11 +1,10 @@
 #include "shell.h"
 
 /**
- * cmd_exit - exit shell
- * @cmd_args: commande
+ * cmd_exit - quitte le programme avec un statut de succes
  * Return: nothing
  */
 void cmd_exit(void)
 {
-exit(EXIT_SUCCESS);
+exit(EXIT_SUCCESS); /* Quitte le programme avec un statut de succes */
 }
