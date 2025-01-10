480be6c27fed2412294dc4e4b485a24528257bf7
Damien353 <10034@holbertonstudents.com>
2025-01-10 09:39:03 +0000

    correction betty style

--- path.c
+++ path.c
@@ -6,9 +6,9 @@
  */
 char *find_command_in_path(char *cmd)
 {
-	char *path = getenv("PATH"); /* Recupere variable d'environnement PATH */
-	char *path_copy, *dir, *full_path; /* Declare des pointeurs pour la copie */
-						/* du PATH, le repertoire et le chemin complet */
+	char *path = getenv("PATH"); /* Recupere variable environnement PATH */
+	char *path_copy, *dir, *full_path; /* Declare pointers pour la copie */
+					/* du PATH, repertoire et full path */
 	size_t len; /* Variable pour la longueur du chemin complet */
 
 	if (!path) /* Verifie si la variable PATH est NULL */
@@ -16,24 +16,24 @@ char *find_command_in_path(char *cmd)
 	path_copy = strdup(path); /* Cree une copie de la variable PATH */
 	if (!path_copy) /* Si la copie echoue, retourne NULL */
 	return (NULL);
-	dir = strtok(path_copy, ":"); /* Divise PATH en repertoire separes par ":" */
+	dir = strtok(path_copy, ":"); /* Divise PATH separe par ":" */
 	while (dir != NULL) /* Tant qu'il y a des repertoires a parcourir */
 	{
-		len = strlen(dir) + strlen(cmd) + 2; /* Calcule la taille totale */
-							/* necessaire pour le chemin complet */
-		full_path = malloc(len); /* Alloue de la memoire pour le chemin complet */
-		if (!full_path) /* Si l'allocation echoue, libere la copie de PATH */
+		len = strlen(dir) + strlen(cmd) + 2; /* Calcul taille totale */
+						/* necessaire pour full path */
+		full_path = malloc(len); /* Alloue memoire pour full path */
+		if (!full_path) /* Si allocation echoue libere copie de PATH */
 	{
 		free(path_copy);
 		return (NULL); /* et retourne NULL */
 	}
-	snprintf(full_path, len, "%s/%s", dir, cmd); /* Concatene le repertoire */
-							/* avec le nom de la commande */
-	if (access(full_path, X_OK) == 0) /* Verifie si le fichier est executable */
+	snprintf(full_path, len, "%s/%s", dir, cmd); /* Concatene repertoire */
+						/* avec nom de la commande */
+	if (access(full_path, X_OK) == 0) /* Verifie si fichier executable */
 	{
 		free(path_copy); /* Libere la copie de PATH */
 		return (full_path); /* Retourne le chemin complet si la */
-					/* commande est trouvee et executable */
+					/* commande trouvee et executable */
 	}
 	free(full_path); /* Si la commande n'est pas trouvee, libere le chemin */
 	dir = strtok(NULL, ":"); /* Passe au repertoire suivant dans PATH */
